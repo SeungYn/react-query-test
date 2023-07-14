f700c5851dd9736bf912398d1214c9b3fdd37143
SeungYn <kwls0407@gmail.com>
2023-07-15 03:42:48 +0900

    remove: counter

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -13,7 +13,6 @@ export default function Test({
 }: {
   revalidate: () => Promise<void>;
 }) {
-  const [count, setCount] = useState(0);
   const router = useRouter();
   const { data, refetch, mutateAsync, mutate } = useTest();
 
@@ -30,8 +29,6 @@ export default function Test({
         Refreshbutton
       </button>
       <p className='text-neutral-100 text-xl'>{JSON.stringify(data)}</p>
-      <p>{count}</p>
-      <button onClick={() => setCount((c) => c + 1)}>countUp</button>
     </div>
   );
 }
